8deedd085801c1befda0fbac022d9141c5dd6a4b
Jessie <jessica.v.rid@gmail.com>
2023-02-02 21:07:52 +0300

    Homework 5 added

---
+++ task1_34_random_3_dig_num/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1 Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange) // Заполняет массив нужным  количесвтом элементов size, от левой границы до правой) 
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)  // Выводит на печать заданный или полученный массив 
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+
+int CountPositiveElements (int [] arr)  // Подсчитывает количество четных чисел в массиве 
+{
+int CountPositiveElements = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] % 2 == 0)
+    {
+        CountPositiveElements = CountPositiveElements + 1;
+    }
+}
+return CountPositiveElements;
+}
+
+int[] array = FillArrayWithRandomNumbers(10, 100, 999);
+PrintArray(array);
+int count = CountPositiveElements(array);
+System.Console.WriteLine($"количество чётных чисел в массиве: {count}");
---
+++ task3_38_double_arr_min_max/Program.cs
@@ -0,0 +1,56 @@
+﻿// //Задача 3 Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+double [] FillArrayWithRandomDoubleNumbers(int size, int leftRange, int rightRange) // Заполняет массив нужным  количесвтом элементов size, от левой границы до правой рандомными  вещественными числами
+{
+    double [] arr = new double[size];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(rand.Next(leftRange, rightRange + 1) + rand.NextDouble(), 2);
+    }
+    return arr;
+}
+
+void PrintArrayDouble(double[] arr)  // Выводит на печать заданный или полученный массив из вещественных чисел 
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+double FindMax(double [] array) // Находит максимальное зн
+{
+double Max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > Max)
+    {
+        Max = array[i];
+    }
+}
+return Max;
+}
+
+double FindMin(double [] array)
+{
+double Min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] < Min)
+    {
+        Min = array[i];
+    }
+}
+return Min;
+}
+
+
+double [] array = FillArrayWithRandomDoubleNumbers (5, 10, 50);
+PrintArrayDouble(array);
+double maximum = FindMax(array);
+System.Console.WriteLine($"Максимальное значение элементов массива: {maximum}");
+double minimum = FindMin(array);
+System.Console.WriteLine($"Минимальное значение элементов массива: {minimum}");
+double result = maximum - minimum;
+System.Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {result}");
