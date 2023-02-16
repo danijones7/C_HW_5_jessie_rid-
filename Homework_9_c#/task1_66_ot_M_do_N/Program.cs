1c988e075b519c6090b32d1abe3bbb47df993565
Jessie <jessica.v.rid@gmail.com>
2023-02-16 17:36:42 +0300

    Homework 9 added

---
+++ Homework_9_c#/task1_66_ot_M_do_N/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputNumber(string str)  // Функция, которая предлагает ввести пользователю число, и возвращает это число, или пишет, что введено некорректное значение 
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        System.Console.WriteLine(str);
+        text = Console.ReadLine();
+
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено некорректное число");
+    }
+    return number;
+}
+
+int SumElements (int m, int n)  // Функция, которая с помощью рекурсии подсчитывает количество элементов в заданном промежутке 
+{    
+   
+    if (m <= n)
+    {        
+        return m + SumElements(m+1, n);
+    }
+    return 0;
+}
+
+
+//Основной код:
+
+int M = InputNumber("Введите значение M: ");
+int N = InputNumber("Введите значение N: ");
+int sum = SumElements(M, N);
+System.Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N} равна: {sum}");
---
+++ Homework_9_c#/task2_68_Akkerman/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int InputNumber(string str)  // Функция, которая предлагает ввести пользователю число, и возвращает это число, или пишет, что введено некорректное значение 
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        System.Console.WriteLine(str);
+        text = Console.ReadLine();
+
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено некорректное число");
+    }
+    return number;
+}
+
+int Akerman(int m, int n)  // Функция Аккермана
+{
+if (m == 0)
+{
+return n+1;
+}
+if (m > 0 && n ==0)
+{
+return Akerman(m-1,1);
+}
+if (m > 0 && n > 0)
+{
+return Akerman(m-1, Akerman(m, n -1));
+}
+return 0;
+}
+
+
+int M = InputNumber("Введите неотрицательное число M: ");
+int N = InputNumber("Введите неотрицательное число N: ");
+int Ak = Akerman(M, N);
+System.Console.WriteLine($"Функция Аккермана A({M},{N}) возвращает число: {Ak}");
